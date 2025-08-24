ea2c7633a7e06b930e71d9d40410eed19d3fa71b
Emre Zeytun <emrezeytun35@192.168.1.7>
2025-08-24 18:19:51 +0300

    fix(button): update button text and build outputs

--- packages/helloWorld/dist/components/index.js
+++ packages/helloWorld/dist/components/index.js
@@ -1,19 +1,5 @@
-import { HelloWorld } from './hello-world.js';
-export { g as getAssetPath, r as render, s as setAssetPath, a as setNonce, b as setPlatformOptions } from './hello-world.js';
-
-const defineCustomElements = (opts) => {
-    if (typeof customElements !== 'undefined') {
-        [
-            HelloWorld,
-        ].forEach(cmp => {
-            if (!customElements.get(cmp.is)) {
-                customElements.define(cmp.is, cmp, opts);
-            }
-        });
-    }
-};
-
-export { defineCustomElements };
+export { getAssetPath, render, setAssetPath, setNonce, setPlatformOptions } from '@stencil/core/internal/client';
+export { HelloWorld, defineCustomElement as defineCustomElementHelloWorld } from './hello-world.js';
 //# sourceMappingURL=index.js.map
 
 //# sourceMappingURL=index.js.map
