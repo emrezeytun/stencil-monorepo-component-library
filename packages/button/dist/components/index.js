3c167a07ea64a40774642e1d285023c2f077d432
Emre Zeytun <emrezeytun35@192.168.1.7>
2025-08-24 18:16:54 +0300

    update

--- packages/button/dist/components/index.js
+++ packages/button/dist/components/index.js
@@ -1,19 +1,5 @@
-import { CompButton } from './comp-button.js';
-export { B as Button, d as defineCustomElement, g as getAssetPath, r as render, s as setAssetPath, a as setNonce, b as setPlatformOptions } from './comp-button.js';
-
-const defineCustomElements = (opts) => {
-    if (typeof customElements !== 'undefined') {
-        [
-            CompButton,
-        ].forEach(cmp => {
-            if (!customElements.get(cmp.is)) {
-                customElements.define(cmp.is, cmp, opts);
-            }
-        });
-    }
-};
-
-export { defineCustomElements };
+export { getAssetPath, render, setAssetPath, setNonce, setPlatformOptions } from '@stencil/core/internal/client';
+export { B as Button, CompButton, d as defineCustomElement, defineCustomElement as defineCustomElementCompButton } from './comp-button.js';
 //# sourceMappingURL=index.js.map
 
 //# sourceMappingURL=index.js.map
